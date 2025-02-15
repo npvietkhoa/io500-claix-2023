13711fe9da920aa0b4befcf199d5f238175b7dd5
npvietkhoa <phuc.viet.khoa.nguyen@rwth-aachen.de>
2025-02-15 20:24:52 +0100

    set account parameter to assigned group

--- io500-slurm-job.sh
+++ io500-slurm-job.sh
@@ -5,13 +5,18 @@
 #SBATCH --time=01:59:00
 #SBATCH --ntasks=16
 #SBATCH --nodes=1
+#SBATCH --account=thes1876
 #SBATCH --output=/hpcwork/ph077533/io500-beeond/io500-beeond_%j.out
-#SBATCH --error=error_%x_%A_%a.err
 
 ml purge
 ml load iimpi
 ml load HDF5
+cp $HPCWORK/io500-claix-2023/io500 $BEEOND
+cp $HPCWORK/io500-claix-2023/config-beeond-mpi.ini $BEEOND
 
-cp -r $HPCWORK/io500-claix-2023/io500 $BEEOND
-cp -r $HPCWORK/io500-claix-2023/config-beeond-mpi.ini $BEEOND
+cd $BEEOND
+ls -l
+file io500
+file config-beeond-mpi.ini
+
 $MPIEXEC ./io500 config-beeond-mpi.ini
